ef62730f2129e27ce31d2b8372001a3201d2e298
Branson Fox <32143148+bransonf@users.noreply.github.com>
2019-10-21 10:49:49 -0500

    download and parse scripts

---
+++ download.R
@@ -0,0 +1,35 @@
+# Download Sources of Data
+
+# Download to Temp Function
+tmp <- tempdir()
+download <- function(url, temp = tmp){
+  download.file(url, file.path(temp, basename(url)))
+}
+
+# Parcels (Shape, used for Key: HANDLE)
+download('https://www.stlouis-mo.gov/data/upload/data-files/prcl_shape.zip')
+
+# Parcels (Information)
+# Includes Tax Billing Information
+download('https://www.stlouis-mo.gov/data/upload/data-files/prcl.zip')
+
+# LRA Inventory
+download('https://www.stlouis-mo.gov/data/upload/data-files/lra_public.zip')
+
+# Building Inspections
+# Includes Condemnations and Vacant buildings
+download('https://www.stlouis-mo.gov/data/upload/data-files/bldginsp.zip')
+
+# Building Permits
+# Includes Occupancy and Demolition
+download('https://www.stlouis-mo.gov/data/upload/data-files/prmbdo.zip')
+
+# Property Maintenance Billing (Using Forestry Property Maintenance)
+download('https://www.stlouis-mo.gov/data/upload/data-files/forestry-maintenance-properties.csv')
+
+
+# Residential Building Structural Information
+# Commercial Building Structural Information
+## (Provided, Laura Ginn)
+
+# Neighborhoods Standard Price List (NOT USED because last update 2009)
---
+++ parse.R
@@ -0,0 +1,40 @@
+# Parse Downloaded Data into R Data Structures
+# (This Script will only run on a Mac/*nix OS, but can be adapted to Windows using the RODBC package)
+library(Hmisc)
+library(foreign)
+library(readxl)
+
+tmp <- tempdir() # Should be the same as in Download.R
+
+# Unzip all files in temp directory
+files <- list.files(tmp, "\\.zip")
+for (i in files) {
+  unzip(file.path(tmp, i), exdir = tmp)
+}
+
+# Parse All .dbf
+for (i in list.files(tmp, "\\.dbf")){
+  assign(i, read.dbf(file.path(tmp, i)))
+}
+
+# Parse All .mdb
+for (i in list.files(tmp, "\\.mdb")){
+  # Check for multiple tables
+  tables <- mdb.get(file.path(tmp, i), tables = TRUE)
+  if(length(tables) > 1){
+    for (j in tables) {
+      assign(j, mdb.get(file.path(tmp, i), tables = j))
+    }
+  }else{
+    assign(i, mdb.get(file.path(tmp, i)))
+  }
+  
+}
+
+# Read Excel Files
+for (i in list.files('data', '\\.xlsx')){
+  assign(i, read_xlsx(file.path('data', i)))
+}
+
+# Save entire Global Environment to Rdata
+save(list = ls(), file = 'data/all.rda')
