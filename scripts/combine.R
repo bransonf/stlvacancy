3a75135e1dfae15c41fbec065f65d6bea4b635c4
Branson Fox <32143148+bransonf@users.noreply.github.com>
2019-11-22 15:42:09 -0600

    refine process

--- scripts/combine.R
+++ scripts/combine.R
@@ -30,11 +30,11 @@ master <- data.frame(stringsAsFactors = FALSE,
 # --------000 Parcel
 
 handle <- function(block, parcel, condo = 0){ # Warning, Not vectorized!
-  block %<>% as.character
+  block %<>%
+    as.character() %>%
+    strsplit('\\.')
   
-
-    block %<>% strsplit('\\.')
-    block = str_pad(block[[1]][1], 4, 'left', '0')
+    blk = str_pad(block[[1]][1], 4, 'left', '0')
     
     if(is.na(block[[1]][2])){
       subblock = "00"
@@ -46,7 +46,7 @@ handle <- function(block, parcel, condo = 0){ # Warning, Not vectorized!
     as.character %>%
     str_pad(3, 'left', '0')
     
-  paste0(1,block,subblock,condo,parcel)
+  paste0(1,blk,subblock,condo,parcel)
 }
 
 # Add Handle for Datasets that need it
@@ -70,12 +70,13 @@ PrmDemo  %<>% add_handle('CityBlock', 'Parcel')
 # Select Variables from Datasets (Can Always Add more back in later...)
 Prcl %<>% transmute(
   Handle = as.character(Handle),
-  BldgVac = VacBldgYear > 2016,
+  BldgVac = VacBldgYear > 2016, # Building Division Survey 2017
   Church = ifelse(AsrClassCode %in% c(19:27, 30:37), TRUE, FALSE),
   AsrVacLot = ifelse(AsrLandUse1 %in% c(1010,9100), TRUE, FALSE),
-  ASrVacBld = ifelse(AsrLandUse1 %in% c(9111,9112,9141,9142,9151,9152,9171,9172,9400), TRUE, FALSE),
-  AsrTrade = ifelse(AsrLandUse1 %in% c(5000,5990,9140,5900,5800,5700,5600,5400,5300,5200,5190,5100), TRUE, FALSE)
-) %>%
+  AsrVacBld = ifelse(AsrLandUse1 %in% c(9111,9112,9141,9142,9151,9152,9171,9172,9400), TRUE, FALSE),
+  AsrTrade = ifelse(AsrLandUse1 %in% c(5000,5990,9140,5900,5800,5700,5600,5400,5300,5200,5190,5100), TRUE, FALSE),
+  Rmv = ifelse(AsrLandUse1 %in% c(7600, 7610, 4500) | CDALandUse1 %in% c(7600, 7610, 4500) ,TRUE, FALSE) # Parks, Highway Right of Way, Green Way(?), Landuse Projects(?)
+  ) %>%
   filter(!duplicated(Handle))
 
 BldgCom %<>% transmute(
@@ -91,13 +92,13 @@ BldgCom %<>% transmute(
                        BldgCategory == 'SV' ~ 'Services'
                        ),
   BldgCom = TRUE
-) %>%
+  ) %>%
   filter(!duplicated(Handle))
 
 BldgRes %<>% transmute(
   Handle,
   BldgRes = TRUE
-) %>%
+  ) %>%
   filter(!duplicated(Handle))
 
 Condemn %<>% transmute(
@@ -122,7 +123,7 @@ Condemn %<>% transmute(
                       InspectType == 'SG' ~ 'Sign',
                       InspectType == 'XX' ~ 'Emergency Board Up'
                       )
-) %>%
+  ) %>%
   filter(!duplicated(Handle))
 
 dbo_vw_public_inventory %<>%
@@ -138,7 +139,7 @@ dbo_vw_public_inventory %<>%
 `forestry-maintenance-properties.csv` %<>% transmute(
   Handle = as.character(HANDLE),
   ForestryType = as.character(PROPERTYTYPE)
-)  %>%
+  )  %>%
   filter(!duplicated(Handle))
 
 VacBldg %<>%
@@ -146,7 +147,7 @@ VacBldg %<>%
   transmute(
   Handle = as.character(Handle),
   VacantSurvery = TRUE
-) %>%
+  ) %>%
   filter(!duplicated(Handle))
 
 PrclREAR %<>%
@@ -220,7 +221,7 @@ master %<>% mutate(
   BldgVac = ifelse(is.na(BldgVac), FALSE, BldgVac),
   Church = ifelse(is.na(Church), FALSE, Church),
   AsrVacLot = ifelse(is.na(AsrVacLot), FALSE, AsrVacLot),
-  ASrVacBld = ifelse(is.na(ASrVacBld), FALSE, ASrVacBld),
+  AsrVacBld = ifelse(is.na(AsrVacBld), FALSE, AsrVacBld),
   AsrTrade = ifelse(is.na(AsrTrade), FALSE, AsrTrade),
   BldgCom = ifelse(is.na(BldgCom), FALSE, BldgCom),
   BldgRes = ifelse(is.na(BldgRes), FALSE, BldgRes),
@@ -233,8 +234,9 @@ master %<>% mutate(
   BoardUp = ifelse(is.na(BoardUp), 0, BoardUp),
   ForServCnt = ifelse(is.na(ForServCnt), 0, ForServCnt),
   VacReg = ifelse(is.na(VacReg), 0, VacReg),
-  VacPnlty = ifelse(is.na(VacPnlty), 0, VacPnlty)
-)
+  VacPnlty = ifelse(is.na(VacPnlty), 0, VacPnlty),
+  Rmv = ifelse(is.na(Rmv), FALSE, Rmv)
+  )
 
 # Compute Other Fields
 master %<>% mutate(
@@ -256,24 +258,25 @@ master %<>% mutate(
     !PrmBldg & AsrVacLot & Church ~ 'Possible Vacant Lot',
     TRUE ~ 'Not Vacant'
   )
-)
+  ) %>%
+  filter(!Rmv) %>%
+  select(-Rmv)
 
-write.csv(filter(master, VacStatus != 'Not Vacant'), file ='vacancy_estimate.csv')
+write.csv(filter(master, VacStatus != 'Not Vacant'), file ='vacancy_estimate.csv', row.names = FALSE)
 
 # > table(master$VacStatus)
 # 
 # Not Vacant   Possible Building Possible Vacant Lot     Vacant Building          Vacant Lot 
-#     102233                  53                7806                6472               11024 
+#     102226                  69                7784                6347               11142 
 
-# Expected (Close to, from the 2018 Dataset)
+# Expected (Close to, from the 2018 Dataset) (- absolute dupes in original)
 #
 # > table(VC$VacCatText)
 # 
 # Possible Vacant Building      Possible Vacant Lot 
-# 377  (-1)                  5718 (-87)
+# 377  (-1)                     5718 (-87)
 # Vacant Building               Vacant Lot 
 # 7663    (-221)                12524 (-390)
-
 
 
 # Classify Vacancy According to Criteria
