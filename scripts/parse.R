da8db50e5956986b47ac8fbf8c1616e0c4ca7d2a
bransonf <32143148+bransonf@users.noreply.github.com>
2019-12-05 16:16:56 -0600

    refactor and lint

--- scripts/parse.R
+++ scripts/parse.R
@@ -1,16 +1,15 @@
 # Parse Downloaded Data into R Data Structures
 # (This Script will only run on a Mac/*nix OS, but can be adapted to Windows using the RODBC package)
-library(Hmisc)
-library(foreign)
-library(readxl)
-library(readr)
-library(sf)
+library(Hmisc)   # for .mdb
+library(foreign) # for .dbf
+library(readxl)  # for .xls*
+library(readr)   # for .csv
+library(sf)      # for .shp
 
 tmp <- tempdir() # Should be the same as in Download.R
 
 # Unzip all files in temp directory
-files <- list.files(tmp, '\\.zip')
-for (i in files) {
+for (i in list.files(tmp, '\\.zip')) {
   unzip(file.path(tmp, i), exdir = tmp)
 }
 
@@ -26,16 +25,11 @@ for (i in list.files(tmp, '\\.shp')) {
 
 # Parse All .mdb
 for (i in list.files(tmp, '\\.mdb')){
-  # Check for multiple tables
+  # Parse Each Table Individually
   tables <- mdb.get(file.path(tmp, i), tables = TRUE)
-  if(length(tables) > 1){
     for (j in tables) {
       assign(j, mdb.get(file.path(tmp, i), tables = j))
     }
-  }else{
-    assign(i, mdb.get(file.path(tmp, i)))
-  }
-  
 }
 
 # Parse All .csv
@@ -48,14 +42,13 @@ for (i in list.files('data', '\\.xlsx')){
   assign(i, read_xlsx(file.path('data', i)))
 }
 
-# Save entire Global Environment to Rdata (After removing some extraneous vars)
-rmv <- c(grep('Cd', ls(), value = TRUE), grep('Date', ls(), value = TRUE),
-         'files', 'i', 'j','tables','tmp')
-rm(rmv, download, list = rmv)
+# Load Encrypted Forestry Data
+source('https://bransonf.com/scripts/encryption.R')
+forestry_services = decrypt_csv('data/Forestry19.csv.encrypted', Sys.getenv('PW'))
+for_codes = readr::read_csv('data/forestry_services.csv')
 
-# Save Par.dbf to own file
-save(par.dbf, file = 'data/par.rda')
-rm(par.dbf)
-
-# Save all else
-save(list = ls(), file = 'data/all.rda')
+# Save Parsed Data
+save(prcl.dbf, Prcl, BldgCom, BldgRes, Condemn, dbo_vw_public_inventory,
+     `forestry-maintenance-properties.csv`, VacBldg, for_codes, forestry_services,
+     PrclREAR, PrmDemo, PrmBldg, PrmOcc,
+     file = 'data/parsed.rda')
